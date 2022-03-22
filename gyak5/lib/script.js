bce1c23c572deb6cc6ee02a1a96c122520f674f5
vegh-barnabas <ak3g4s@inf.elte.hu>
2022-03-22 12:36:04 +0000

    Add lecture 5 files

---
+++ gyak5/lib/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var o = {
+  a: 1,
+  b: 0
+};
+var c = o.a,
+    b = o.b;
+console.log(c);
---
+++ gyak5/src/script.js
@@ -0,0 +1,8 @@
+const o = {
+    a: 1,
+    b: 0
+};
+
+const { a: c, b } = o
+
+console.log(c);
